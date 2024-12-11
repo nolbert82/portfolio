1885485dad0784df489aa42b9fa1ba94c4e59d03
Nolan GILBERT <nolanofrance@gmail.com>
2024-12-11 22:17:28 +0100

    Add portfolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,5 +5,12 @@ export const projects = [
     description: "A website where you can play the game 2048, and also get help by an AI.",
     technologies: ["Python", "TensorFlow", "JavaScript"],
     demo: "https://poetic-crumble-896d60.netlify.app/"
+  },
+  {
+    id: 2,
+    title: "PortFolio website",
+    description: "A collection of my projects. Don't click or you will be stuck in an endless loop.",
+    technologies: ["TypeScript", "React", "Vite"],
+    demo: "https://portfolio-nolan-gilbert.vercel.app/"
   }
 ];
