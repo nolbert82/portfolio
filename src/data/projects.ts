b534dbc5029d171e22dfef1240c61fb377f89395
Nolan GILBERT <115945238+nolbert82@users.noreply.github.com>
2025-01-05 15:21:13 +0100

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,14 +2,14 @@ export const projects = [
   {
     id: 1,
     title: "AI-Powered 2048",
-    description: "A website where you can play the game 2048, and also get help by an AI.",
-    technologies: ["Python", "TensorFlow", "JavaScript"],
+    description: "A website where you can play the game 2048, and get helped by an AI.",
+    technologies: ["TypeScript", "Expectimax"],
     demo: "https://poetic-crumble-896d60.netlify.app/"
   },
   {
     id: 2,
     title: "PortFolio website",
-    description: "A collection of my projects. Don't click or you will be stuck in an endless loop.",
+    description: "A collection of my projects. Don't click or you will get stuck in an endless loop.",
     technologies: ["TypeScript", "React", "Vite"],
     demo: "https://portfolio-nolan-gilbert.vercel.app/"
   }
